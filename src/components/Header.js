21befd907e5a2d19f2df97060d803fbf9b81ceec
kaloyan-kostadinov-mm <kaloyan.kostadinov@mentormate.com>
2023-05-25 11:03:04 +0300

    minor refinements

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Logo from '../assets/logo_copy.png'
-import Video from '../assets/reel.mp4'
+import Video from '../assets/reel_converted.mov'
+import Thumb from '../assets/thumb.png'
 import './styles/styles.css';
 
 export const Header = () => {
@@ -15,13 +16,19 @@ export const Header = () => {
             <header>
                 <a href="#home"><img src={Logo} className="logo" alt="Logo" /></a>
             </header>
-
+            <img
+                src={Thumb}
+                className="video-thumb tiny"
+                alt="thumb"
+                style={{ opacity: isVideoLoaded ? 0 : 1 }}
+            />
             <video
                 src={Video}
                 muted
                 loop
                 autoPlay
-                onLoadedData={onLoadedData}
+                playsInline
+                onLoadedData={() => onLoadedData()}
                 style={{ opacity: isVideoLoaded ? 1 : 0 }}
             />
 
